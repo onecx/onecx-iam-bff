436ebab843fc629afe75f3ae3048a9446bbaa54d
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-04-23 15:17:10 +0200

    feat: added new exception handler

--- src/main/java/org/tkit/onecx/iam/bff/rs/controllers/RolesRestController.java
+++ src/main/java/org/tkit/onecx/iam/bff/rs/controllers/RolesRestController.java
@@ -3,10 +3,10 @@ package org.tkit.onecx.iam.bff.rs.controllers;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.validation.ConstraintViolationException;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.ClientWebApplicationException;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.tkit.onecx.iam.bff.rs.mappers.ExceptionMapper;
@@ -46,7 +46,7 @@ public class RolesRestController implements RolesInternalApiService {
     }
 
     @ServerExceptionMapper
-    public Response restException(WebApplicationException ex) {
-        return Response.status(ex.getResponse().getStatus()).build();
+    public Response restException(ClientWebApplicationException ex) {
+        return exceptionMapper.clientException(ex);
     }
 }
